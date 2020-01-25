79a1fb3d7bdfaac02cff82742803a147af158bc1
Mike <maboals@gmail.com>
2020-01-25 11:19:04 -0800

    startup2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Project_03
 Metis Bootcamp Project 3
+
+<h1> Classification Project </h1>
---
+++ src/data_import.py
@@ -0,0 +1,26 @@
+'''
+    File: data_import.py
+
+    Description:
+        Tools to read data for Project 3 - Classification
+
+    Author:
+
+    Date Created:
+
+    Date Modified:
+
+    Update Log:
+
+    Functions Defined:
+
+
+'''
+
+VERSION_STRING = '0.1'
+
+def version()
+    return VERSION_STRING
+
+
+
